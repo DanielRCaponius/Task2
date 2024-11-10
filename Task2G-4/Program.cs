8f035cde001a832ba7c354d27b024b8ac5338a86
DanielRCaponius <146859076+DanielRCaponius@users.noreply.github.com>
2024-11-10 18:51:37 +0300

    Add files via upload

---
+++ Task2G-4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task2G_4{
+    class Program{
+        static void Main(string[] args){
+            bool a = true;
+            double b = 3.14;
+
+            Console.WriteLine($"{(a,b)}");
+            Console.WriteLine(swap_it(a,b));
+        }
+
+        static object swap_it(object a, object b){
+            object a_new = b;
+            object b_new = a;
+            
+            return (a_new, b_new); //Возврат значений по заветам Горюнова.
+        }
+    }
+}
