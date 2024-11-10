d1a8065d23182335d1204b19b9d11c0197ba9bc4
DanielRCaponius <146859076+DanielRCaponius@users.noreply.github.com>
2024-11-10 19:20:30 +0300

    Add files via upload

---
+++ Task2G-5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices.ObjectiveC;
+
+namespace Task2G_5{
+    class Program{
+        static void Main(string[] args){
+            ListStorage A = new ListStorage();
+            A.Add(3);
+            A.Add(true);
+            A.Add("./'[1/1+x^2]dx = tan^-1(x)+C");
+            A.Add(0x5f3759df);
+
+            A.write_list();
+
+            A.Get(2);
+        }
+    }
+
+    interface IStorage{
+        void Add(){}
+        void Get(){}
+    }
+
+    class ListStorage : IStorage{
+        
+        List<object> list = new List<object>();
+        
+        public void Add(object new_obj){
+            list.Add(new_obj);
+        }
+
+        public void write_list(){
+            foreach(object a in list){
+                Console.WriteLine(a);
+            }
+        }
+
+        public void Get(int index){
+            Console.WriteLine(list[index]);
+        }
+    }
+}
