fce73ba1524d9c93b8a3ebdc66ac473bef5f0849
DanielRCaponius <146859076+DanielRCaponius@users.noreply.github.com>
2024-11-10 14:57:50 +0300

    Add files via upload

---
+++ Task2D-5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task2D_5{
+    class Program{
+        static void Main(string[] args){
+            Logger A = new Logger();
+            A.Go();
+        }
+    }
+    abstract class ILogger{
+        void Go(){
+        }
+    }
+
+    class Logger : ILogger{
+        public void Go(){
+            Console.WriteLine("Логгер идёт...");
+        }
+    }
+}
